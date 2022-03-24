1641a630cfe5f5947daf2a947a662d055b06a598
Gabriel Barros <gabrielbarrosdeo@hotmail.com>
2022-03-23 21:56:35 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Equipe = (props: any) =>{
+  return(
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+      <Social fb={props.facebook} />
+    </div>
+  )
+}
+
+const Sobre = (props:any) =>{
+  return(
+    <div>
+      <h2>Olá sou o(a) {props.nome}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>Idade: {props.idade}</h3>
+    </div>
+  )
+}
+
+const Social = (props:any) =>{
+  return(
+    <div>
+      <a href={props.fb}>Facebook</a>
+      <a>LinkedIn</a>
+      <a>Youtube</a>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Conheça nossa equipe!</h1>
+      <Equipe nome='Lucas' cargo='Programador' idade='29' facebook="google.com"/>
+      <Equipe nome='Matheus' cargo='Designer' idade='18'/>
     </div>
   );
 }
